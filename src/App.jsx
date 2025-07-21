515d652bb2ed21d58b81218499eea92b0703a621
Wadad Parker <wadadparker5@gmail.com>
2025-07-21 08:57:19 +0530

    feat: streaming

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import ChatBox from "./components/ChatBox"
 import ChatResponse from "./components/ChatResponse";
 
 function App() {
+  const [streamMode,setStreamMode] = useState(false);
   const [chatResponse,setChatResponse] = useState("");
 
   return (
     <div className="flex flex-col-reverse justify-start items-center gap-y-10 h-screen pb-2">
-      <ChatBox setChatResponse={setChatResponse}/>
+      <button onClick={()=>setStreamMode(prev=>!prev)} className={` fixed top-2 right-4 border rounded-lg px-2 py-1 ${streamMode?"text-white bg-black":""} transition-all ease-linear hover:cursor-pointer `}>Stream Mode</button>
+      <ChatBox streamMode={streamMode} setChatResponse={setChatResponse}/>
       <ChatResponse chat={chatResponse} />
       <h1 className="sticky top-0 text-9xl text-center">CatChat</h1>
     </div>
--- src/components/ChatBox.jsx
+++ src/components/ChatBox.jsx
@@ -1,7 +1,7 @@
-import { useEffect, useState } from "react"
+import { useState } from "react"
 
 
-const ChatBox = ({setChatResponse}) => {
+const ChatBox = ({streamMode,setChatResponse}) => {
     const [input,setInput] = useState("");
     const [isLoading,setIsLoading] = useState(false);
 
@@ -16,13 +16,48 @@ const ChatBox = ({setChatResponse}) => {
                 body:JSON.stringify({message:input})
             })
             const data = await response.json();
-            setIsLoading(false);
+            
             setChatResponse(data.reply);
         }
         catch (error) {
             console.log("There was an error calling the API",error);
-            setIsLoading(false);
             setChatResponse("There was an error calling the API.")
+        }
+        finally {
+            setIsLoading(false);
+        }
+    }
+
+    const callStreamService = async () => {
+        setIsLoading(true); 
+        try {
+            const response = await fetch("http://localhost:3000/api/stream", {
+                method: "POST",
+                headers: {
+                    "Content-Type":"application/json"
+                },
+                body: JSON.stringify({ message: input })
+            })
+
+            const reader = response.body.getReader();
+            const decoder = new TextDecoder();
+
+            let streamCompleted = false;
+            while (!streamCompleted) {
+                const { value, streamCompleteStatus } = await reader.read();
+                streamCompleted = streamCompleteStatus;
+                if(value) {
+                    const chunk = decoder.decode(value);
+                    setChatResponse(prev=> prev + chunk)
+                }
+            }
+        }
+        catch (error) {
+            console.error('Stream error:', err);
+            setChatResponse('Error: ' + err.message);
+        }
+        finally {
+            setIsLoading(false);
         }
     }
 
@@ -30,7 +65,7 @@ const ChatBox = ({setChatResponse}) => {
     <>
     <section className="relative w-screen justify-start items-center flex flex-col h-fit ">
         <textarea disabled={isLoading} value={input} onChange={(e)=>setInput(e.target.value)} placeholder="Ask anything to our Cat Expert!" className="text-lg border w-1/2 h-40 rounded-xl p-4 resize-none" />
-        <button disabled={isLoading} onClick={callChatService} className="absolute bottom-2 right-1/4 mr-3 z-30 text-xs border rounded-md px-2 hover:cursor-pointer disabled:opacity-50 disabled:hover:cursor-default">Generate!</button>
+        <button disabled={isLoading} onClick={()=>streamMode?callStreamService():callChatService()} className="absolute bottom-2 right-1/4 mr-3 z-30 text-xs border rounded-md px-2 hover:cursor-pointer disabled:opacity-50 disabled:hover:cursor-default">Generate!</button>
     </section>    
     {isLoading && <div className="border-2  border-r-transparent border-black size-5 rounded-full animate-spin mb-20"></div>}
     </>
